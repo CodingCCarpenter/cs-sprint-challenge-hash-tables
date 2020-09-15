2ef7f9b5958e95122bf11848c99f07e251b923aa
CodingCCarpenter <Christineassists@aol.com>
2020-09-14 21:43:53 -0500

    solve ex5.py

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -6,7 +6,21 @@ def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    table = {}
+
+    for path in files:
+        parts = path.split("/")
+
+        if table.get(parts[-1]):
+            table[parts[-1]].append(path)
+        else:
+            table[parts[-1]] = [path]
+
+            result = []
+
+    for val in queries:
+        if table.get(val):
+            result.extend(table.get(val))
 
     return result
 
